f112c6142c920878bd7ff865f4682c42ea6bbd38
UI Cats <ignacychatgpt@gmail.com>
2026-04-13 18:29:23 +0200

    Add files via upload

---
+++ NewControls/Btn.Designer.cs
@@ -0,0 +1,22 @@
+﻿namespace NewControls
+{
+    partial class Btn
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+
+        #endregion
+    }
+}
