eae61242f3679729aaadb54d4b8ea80a6a3a905e
UI Cats <ignacychatgpt@gmail.com>
2026-05-11 18:18:14 +0200

    Add files via upload

---
+++ NewControls/Dialog/ActionDialog.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NewControls.Dialog
+{
+    public class ActionDialog : Dialog
+    {
+        private string _txt;
+        private Dictionary<CmdLink, Action> _actions;
+        private Font _txtFont;
+        private Color _txtFgColor;
+        private Color _txtBgColor;
+
+        private Label _txtLbl;
+        private Panel _actionsPanel;
+
+        [Category("Appearance")]
+        public string Text
+        {
+            get => _txt;
+            set
+            {
+                _txt = value;
+                _txtLbl.Text = _txt;
+            }
+        }
+
+        [Category("Behavior")]
+        public Dictionary<CmdLink, Action> Actions
+        {
+            get => _actions;
+            set
+            {
+                _actions = value;
+                UpdateCmdLinks();
+            }
+        }
+
+        [Category("Appearance")]
+        public Font TextFont
+        {
+            get => _txtFont;
+            set
+            {
+                _txtFont = value;
+                _txtLbl.Font = _txtFont;
+            }
+        }
+
+        [Category("Appearance")]
+        public Color TextForeColor
+        {
+            get => _txtFgColor;
+            set
+            {
+                _txtFgColor = value;
+                _txtLbl.ForeColor = _txtFgColor;
+            }
+        }
+
+        [Category("Appearance")]
+        public Color TextBackColor
+        {
+            get => _txtBgColor;
+            set
+            {
+                _txtBgColor = value;
+                _txtLbl.BackColor = _txtBgColor;
+            }
+        }
+
+        public ActionDialog(string title, string text, Dictionary<CmdLink, Action> actions, bool canClose = true) : base(title, canClose)
+        {
+            _form.AutoSize = true;
+            _form.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            
+            MinSize = new Size(350, 0);
+            _form.Padding = new Padding(20);
+
+            _txtLbl = new Label()
+            {
+                Dock = DockStyle.Top,
+                TextAlign = ContentAlignment.MiddleLeft,
+                AutoSize = true,
+                Margin = new Padding(0, 0, 0, 15)
+            };
+
+            _actionsPanel = new Panel()
+            {
+                Dock = DockStyle.Top,
+                AutoSize = true,
+                Width = 310
+            };
+
+            _form.Controls.Add(_actionsPanel);
+            _form.Controls.Add(_txtLbl);
+
+            Text = text;
+            Actions = actions;
+        }
+
+        private void UpdateCmdLinks()
+        {
+            _actionsPanel.Controls.Clear();
+            if (_actions == null) return;
+
+            List<CmdLink> links = new List<CmdLink>(_actions.Keys);
+            links.Reverse();
+
+            foreach (var lnk in links)
+            {
+                Action action = _actions[lnk];
+
+                lnk.Dock = DockStyle.Top;
+                lnk.Height = string.IsNullOrEmpty(lnk.Note) ? 35 : 55;
+                lnk.Margin = new Padding(0, 0, 0, 10);
+
+                lnk.Click += (sender, e) => action();
+
+                _actionsPanel.Controls.Add(lnk);
+            }
+
+            _form.PerformLayout();
+        }
+    }
+}
---
+++ NewControls/Dialog/MsgBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NewControls
+{
+    public class MsgBox
+    {
+        public static DialogResult Info(string msg, string title, MessageBoxButtons btns = MessageBoxButtons.OK) => MessageBox.Show(msg, title, btns, MessageBoxIcon.Information);
+        public static DialogResult Ask(string msg, string title, MessageBoxButtons btns = MessageBoxButtons.YesNo) => MessageBox.Show(msg, title, btns, MessageBoxIcon.Question);
+        public static DialogResult Warn(string msg, string title, MessageBoxButtons btns = MessageBoxButtons.OKCancel) => MessageBox.Show(msg, title, btns, MessageBoxIcon.Warning);
+        public static DialogResult Err(string msg, string title, MessageBoxButtons btns = MessageBoxButtons.RetryCancel) => MessageBox.Show(msg, title, btns, MessageBoxIcon.Error);
+        public static DialogResult Msg(string msg, string title, MessageBoxButtons btns) => MessageBox.Show(msg, title, btns, MessageBoxIcon.None);
+        public static DialogResult RetryErr(string msg, Action retryAction, string title = "Error")
+        {
+            DialogResult result = Err(msg, title);
+            if (result == DialogResult.Retry) retryAction();
+            return result;
+        }
+        public static DialogResult RetryErrFromEx(Exception ex, Action retryAction, string title = "Error") => RetryErr(ex.Message, retryAction, title);
+    }
+}
