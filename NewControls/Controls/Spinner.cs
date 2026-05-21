e20c1adb47e5ea423237b19db7a689df455e21b0
UI Cats <ignacychatgpt@gmail.com>
2026-05-21 14:02:41 +0200

    Add files via upload

---
+++ NewControls/Controls/Spinner.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace NewControls.Controls
+{
+    public class Spinner : Control
+    {
+        private System.Windows.Forms.Timer timer;
+        private float angle = 90;
+        private float sweep = 30;
+        private float currentSpeed = 5;
+        private bool expanding = true;
+
+        [Category("Behavior")]
+        public bool Expandable { get; set; } = true;
+
+        [Category("Behavior")]
+        public float Speed { get; set; } = 1.0f;
+
+        private float DefaultSpeed => 5.0f;
+
+        public Spinner()
+        {
+            DoubleBuffered = true;
+            Size = new Size(50, 50);
+            timer = new System.Windows.Forms.Timer
+            {
+                Interval = 20
+            };
+            timer.Tick += UpdateAnimation;
+            timer.Start();
+        }
+
+        private void UpdateAnimation(object sender, EventArgs e)
+        {
+            float baseSpeed = DefaultSpeed * Speed;
+
+            if (Expandable)
+            {
+                if (expanding)
+                {
+                    sweep += baseSpeed;
+                    currentSpeed = Math.Max(1, baseSpeed * (1 - (sweep / 270)));
+                    if (sweep >= 270) expanding = false;
+                }
+                else
+                {
+                    sweep -= (baseSpeed * 0.4f);
+                    currentSpeed = baseSpeed;
+                    if (sweep <= 30) expanding = true;
+                }
+                angle += currentSpeed;
+            }
+            else
+            {
+                sweep = 120;
+                angle += baseSpeed;
+            }
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rect = new Rectangle(5, 5, this.Width - 10, this.Height - 10);
+            using (Pen pen = new Pen(Color.DodgerBlue, 4))
+            {
+                pen.StartCap = LineCap.Round;
+                pen.EndCap = LineCap.Round;
+                e.Graphics.DrawArc(pen, rect, angle, sweep);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                timer.Stop();
+                timer.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- NewControls/NewControls.cs
+++ NewControls/NewControls.cs
@@ -2,7 +2,7 @@
 {
     public class NewControls
     {
-        public static readonly string Ver = "v2.2.1";
-        public static readonly string Changelog = "# `NewControls` Changelog\n\n## Bugfixes\n- Repaired `CmdLink` control size again in `ActionDialog` class\n\n## New Features\n- Added `ActionDialog` to `Action` class in `ActionDialog` class\n";
+        public static readonly string Ver = "v2.3.1";
+        public static readonly string Changelog = "# `NewControls` Changelog\n\n## New Features\n- Added 'Spinner` control class to `Controls` namespace.\n";
     }
 }
